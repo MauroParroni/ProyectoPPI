30d0cefa39db87d883283335ae0f84ed674ea17e
Fran9001 <franco.1998@outlook.com>
2023-11-08 23:27:51 -0300

    Arreglos login

--- script.js
+++ script.js
@@ -1,41 +1,109 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector(".Formulario_registro");
+    // REGISTRARSE
+    const form = document.querySelector(".Formulario_registro");
+  
+    if (form) {
+        form.addEventListener("submit", function (e) {
+        e.preventDefault();
+    
+        const DNI = document.getElementById("DNI").value;
+        const nombre = document.getElementById("nombre").value;
+        const apellido = document.getElementById("apellido").value;
+        const telefono = document.getElementById("telefono").value;
+        const email = document.getElementById("email").value;
+        const contraseña = document.getElementById("contraseña").value;
+    
+        fetch("http://localhost:3000/registrar", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+            DNI,
+            nombre,
+            apellido,
+            telefono,
+            email,
+            contraseña,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+            if (data.success) {
+                alert("Registro exitoso");
+                form.reset();
+                window.location.href = "../index.html";
+            } else {
+                alert("Error en el registro");
+            }
+            })
+            .catch((error) => {
+            console.error("Error: ", error);
+            });
+        });
+    }
+    // INICIAR SESIÓN
+    const loginForm = document.querySelector(".loginForm");
+  
+    if (loginForm) {
+        loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+    
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+    
+        fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+            email,
+            password,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+            if (data.success) {
+                alert("Inicio de sesión exitoso");
 
-  form.addEventListener("submit", function (e) {
-      e.preventDefault();
+                window.location.href = "../index.html";
 
-      const DNI = document.getElementById("DNI").value;
-      const nombre = document.getElementById("nombre").value;
-      const apellido = document.getElementById("apellido").value;
-      const telefono = document.getElementById("telefono").value;
-      const email = document.getElementById("email").value;
-      const contraseña = document.getElementById("contraseña").value;
+                localStorage.setItem('usuarioLogueado', 'true');
+                
+            } else {
+                alert("Error en el inicio de sesión. Verifica tus credenciales.");
+            }
+            })
+            .catch((error) => {
+            console.error("Error: ", error);
+            });
+        });
+    }
 
-      fetch("http://localhost:5000/registrar", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-              DNI,
-              nombre,
-              apellido,
-              telefono,
-              email,
-              contraseña
-          }),
-      })
-          .then((response) => response.json())
-          .then((data) => {
-              if (data.success) {
-                  alert("Registro exitoso");
-                  form.reset();
-              } else {
-                  alert("Error en el registro");
-              }
-          })
-          .catch((error) => {
-              console.error("Error: ", error);
-          });
+    const ingresarBtn = document.getElementById("BtnIngresar");
+    const registrarseBtn = document.getElementById("BtnRegistrarme");
+    const cerrarSesionBtn = document.getElementById("BtnCerrarSesion");
+
+    const usuarioLogueado = localStorage.getItem('usuarioLogueado');
+
+    if (usuarioLogueado === 'true') {
+
+        cerrarSesionBtn.style.display = "block";
+        ingresarBtn.style.display = "none";
+        registrarseBtn.style.display = "none";
+    } else {
+
+        cerrarSesionBtn.style.display = "none";
+        ingresarBtn.style.display = "block";
+        registrarseBtn.style.display = "block";
+    }
+
+    cerrarSesionBtn.addEventListener("click", function () {
+        
+        localStorage.removeItem('usuarioLogueado');
+
+        window.location.href = "index.html";
+    });
   });
-});
+  
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const app = express();
-const port = 5000;
+const port = 3000;
 
 const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 
+// Crear una única instancia de la base de datos
 const db = new sqlite3.Database("database.db");
+
+// Middleware para proporcionar la instancia de la base de datos en las rutas
+app.use((req, res, next) => {
+    req.db = db;
+    next();
+});
 
 db.run(
     "CREATE TABLE IF NOT EXISTS registros (DNI TEXT, nombre TEXT, apellido TEXT, telefono TEXT, email TEXT, contraseña TEXT)",
@@ -22,7 +29,7 @@ db.run(
 app.post("/registrar", (req, res) => {
     const { DNI, nombre, apellido, telefono, email, contraseña } = req.body;
 
-    db.run(
+    req.db.run(
         "INSERT INTO registros (DNI, nombre, apellido, telefono, email, contraseña) VALUES (?, ?, ?, ?, ?, ?)",
         [DNI, nombre, apellido, telefono, email, contraseña],
         (err) => {
@@ -36,9 +43,28 @@ app.post("/registrar", (req, res) => {
     );
 });
 
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+
+    req.db.get(
+        "SELECT email, contraseña FROM registros WHERE email = ? AND contraseña = ?",
+        [email, password],
+        (err, row) => {
+            if (err) {
+                console.error("Error al iniciar sesión:", err);
+                res.json({ success: false });
+            } else if (row) {
+                res.json({ success: true });
+            } else {
+                res.json({ success: false });
+            }
+        }
+    );
+});
+
 app.get("/", (req, res) => {
     res.send("Ejecución correcta.");
-  });
+});
 
 app.listen(port, () => {
     console.log(`Servidor en ejecución en http://localhost:${port}`);
